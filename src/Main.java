0961e23fdbfc1270c63302872cd45bfb40373682
SmthInUrEye <dragon-pg@yandex.ru>
2024-09-26 23:19:37 +0300

    Homework7

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,34 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf ( "Hello and welcome!" );
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println ( "i = " + i );
+        System.out.println ( "Задача №1" );
+        int money = 0;
+        int i = 0;
+        while (money <= 2459000) {
+            money = money + 15000;
+            i++;
+            System.out.println ( "Месяц " + i + ", сумма накоплений равна " + money + "рублей" );
+        }
+
+        System.out.println ( "Задача №2" );
+        i = 1;
+        while (i <= 10) {
+            System.out.print ( " " + i );
+            i++;
+        }
+        System.out.println ( "\n" );
+        while (i > 1) {
+            i--;
+            System.out.print ( " " + i );
+        }
+
+        System.out.println ( "Задача №3" );
+        int population = 12000000;
+        for (int years = 1; i <= 10; i++) {
+            population += population * (17 - 8) / 1000;
+            System.out.println ( "Год " + i + ", численность населения составляет " + population );
         }
     }
+
 }
